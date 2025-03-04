420f191e229fd42d195e4e8ff9eac7b4a7c57363
Yathish K <kyathishkyath4@gmail.com>
2025-03-04 11:23:06 +0530

    Update personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,18 @@
-Update this file to add your code
-name - YATHISH K
-register_number -22IT057
-department - B.Tech-IT
+from flask import Flask, jsonify # type: ignore
+
+app = Flask(__name__)
+
+@app.route('/name', methods=['GET'])
+def get_name():
+    return jsonify({"name": "Yathish"})
+
+@app.route('/register_number', methods=['GET'])
+def get_register_number():
+    return jsonify({"register_number": "22IT057"})
+
+@app.route('/department', methods=['GET'])
+def get_department():
+    return jsonify({"department": "IT"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
